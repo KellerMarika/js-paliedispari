cedc221647bb058286092080b61a81e963a0c51b
KellerMarika <marikakeller92@gmail.com>
2022-11-09 00:10:12 +0100

    start

---
+++ main.js
@@ -0,0 +1,10 @@
+/* PALINDROMIA */
+//ho bisogno di una funzione generica che
+//recuperi il dato inserito dall'utente
+// convalidi il dato inserito dall'utente ---sennò return
+//converta il valore in array
+//ne crei un clone i cui i caratteri sono invertiti
+//controlli che il valore del primo array sia === a quello del secondo array
+//return true o false
+
+//fuori dalla funzione dichiaro il risultato
